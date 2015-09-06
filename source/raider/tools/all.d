788b74ef8f5682ee63471bb7066d6aca9a9850da
Raiderium <raiderium.neo@gmail.com>
2015-09-06 21:57:32 +1000

    Package convention

--- source/raider/tools/all.d
+++
@@ -1,8 +0,0 @@
-module raider.tools.all;
-
-public import raider.tools.array;
-public import raider.tools.looper;
-public import raider.tools.map;
-public import raider.tools.packable;
-public import raider.tools.reference;
-public import raider.tools.stream;
---
+++ source/raider/tools/package.d
@@ -0,0 +1,11 @@
+module raider.tools;
+
+public
+{
+	import raider.tools.array;
+	import raider.tools.looper;
+	import raider.tools.map;
+	import raider.tools.packable;
+	import raider.tools.reference;
+	import raider.tools.stream;
+}
